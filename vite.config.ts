a249cdb35c0b7bbd6c876a67989362a71aa11436
Krnsand <krnsandin@gmail.com>
2026-02-11 10:51:12 +0100

    Add favicon and homepage saved icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,41 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.pub/favicon.ico",
+        "favicon.pub/apple-touch-icon.png"
+      ],
+
+      manifest: {
+        name: "Min App",
+        short_name: "MinApp",
+        description: "Min Solid PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/favicon.pub/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/favicon.pub/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
+
+
